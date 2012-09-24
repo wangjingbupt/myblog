6d27e94ec78fe9cdbbf61e9ddb8db94fe36d9fd9
wangjingbupt <wangjingbupt@139.com>
2012-09-24 14:29:26 +0800

    update callback url

--- weibo/config.php
+++ weibo/config.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '628803579' );
 define( "WB_SKEY" , '53cc7c93724ae3fa4a39391f0d76d78c' );
-define( "WB_CALLBACK_URL" , 'http://lxsnow.me/callback' );
+define( "WB_CALLBACK_URL" , 'http://lxsnow.me/weibo/callback.php' );
