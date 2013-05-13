9187482d03d214ca039c0e2332019b4a681bd975
erik <wangjingbupt@gamil.com>
2013-05-13 21:40:00 +0800

    update

--- daemon/weibo_photo.php
+++ daemon/weibo_photo.php
@@ -1,10 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors',1);
-//include('../config/config.php');
-//include(UTIL.'/Curl.class.php');
-//include(CONFIG.'/db.conf.php');
-//include(UTIL.'/weibo/saetv2.ex.class.php');
+include('../config/config.php');
+include(CONFIG.'/db.conf.php');
 include('../util/weibo/saetv2.ex.class.php');
 
 function connMongo($dbName = 'blog')
